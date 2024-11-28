a7a453b402a3650f7d0872462b8186764008282a
Elabyad247 <elabyad247@gmail.com>
2024-11-28 16:01:43 +0200

    task

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk
+
+WORKDIR /test1
+
+COPY soli.java .
+
+RUN javac soli.java
+
+CMD java soli 
---
+++ soli.java
@@ -0,0 +1,5 @@
+public class elabyad {
+    public static void main(String[] args) {
+        System.out.println("Soliman");
+    }
+} 
